1cb26454a8c001dc1dd441146a30bd9d6ce3e7a6
swaggyzezin <josecarlosrodrigues1113@gmail.com>
2023-03-28 21:58:28 -0300

    ataque funcional

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -59,11 +59,12 @@ class BigMonster extends Character {
 }
 
 class Stage {
-    constructor(fighter1, fighter2, fighter1El, fighter2El) {
+    constructor(fighter1, fighter2, fighter1El, fighter2El, logObject) {
         this.fighter1 = fighter1;
         this.fighter2 = fighter2;
         this.fighter1El = fighter1El;
         this.fighter2El = fighter2El;
+        this.log = logObject;
     }
 
     start() {
@@ -75,18 +76,55 @@ class Stage {
 
     update() {
         //figther1
-        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`;
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`;
         let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
         this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`;
         //figther2
-        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`;
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
         let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
         this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`;
     }
 
-    doAtack(atacking, atacked) {
-        console.log(`${atacking.name} está atacando ${atacked.name}`);
+    doAtack(attacking, attacked) {
+        if(attacking.life <= 0 || attacked.life <= 0) {
+            this.log.addMgs("Atacando cachorro morto.");
+            return;
+        }
+
+        let attackFactor = (Math.random() * 2).toFixed(2);
+        let defenseFactor = (Math.random() * 2).toFixed(2);
+
+        let actualAttack = attacking.atack * attackFactor;
+        let actualDefense = attacked.defense * defenseFactor;
+        
+        if(actualAttack > actualDefense){
+            attacked.life -= actualAttack;
+            this.log.addMgs(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}!`);
+        } else {
+            this.log.addMgs(`${attacked.name} defendeu!`);
+        }
 
         this.update();
+    }
+}
+
+class Log {
+    list = [];
+
+    constructor(listEl) {
+        this.listEl = listEl;
+    }
+
+    addMgs(msg) {
+        this.list.push(msg)
+        this.render()
+    }
+
+    render(){
+        this.listEl.innerHTML = '';
+
+        for(let i in this.list) {
+            this.listEl.innerHTML += `<li>${this.list[i]}</li>`;
+        }
     }
 }
